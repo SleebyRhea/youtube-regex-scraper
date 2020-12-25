96a6300476420b393a0da43b50b9af241fc337ae
Andrew Wyatt <sleepyfugu@gmail.com>
2020-12-24 19:47:45 -0500

    Clean up the code, regex is now configurable

--- scrape.py
+++ scrape.py
@@ -1,15 +1,23 @@
-#!/usr/local/env python
+#!/usr/bin/env python
 import os
 import re
 import sys
 import json
-from googleapiclient.discovery import build
-import googleapiclient.errors
-import google.auth.exceptions
+import argparse
 
-max_results = 10
+try:
+  import googleapiclient.discovery as gapi 
+  import googleapiclient.errors
+  import google.auth.exceptions
 
-def make_request(req: googleapiclient.discovery.HttpRequest):
+except ImportError as err:
+  print("Failed to import a required dependency: {0}".format(err))
+  exit(1)
+
+program_description = "Scrape Youtube video descriptions using a regex"
+
+def make_request(req: gapi.HttpRequest):
+  """Execute a Google Api request return it's response"""
   try:
     res = req.execute()
 
@@ -25,89 +33,29 @@ def make_request(req: googleapiclient.discovery.HttpRequest):
   return res
 
 
-def main(api: googleapiclient.discovery.Resource, id: str):
-  videos = []
-  video_data = []
-
-  print("Starting scrape", file=sys.stderr)
-  print("Grabbing channel: %s" % id, file=sys.stderr)
-  channel_data = make_request(api.channels().list(
-    part="id,contentDetails",
-    id="UCLBZcndRzbN92awmkpuTmDg" 
-  ))
-
-  playlist_id = channel_data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-
-  print("Grabbing playlist ID: %s" % playlist_id, file=sys.stderr)
-  playlist_data = make_request(api.playlistItems().list(
-    playlistId=playlist_id,
-    part='contentDetails',
-    maxResults=max_results
-  ))
-
-  next_page     = playlist_data['nextPageToken']
-  total_results = playlist_data['pageInfo']['totalResults']
-  results_per   = playlist_data['pageInfo']['resultsPerPage']
-  total_pages   = total_results // results_per
-
-  if total_results < 1:
-    print("That channel has no uploaded videos", file=sys.stderr)
-    exit(0)
-
-  # Just append to an array. We can clean this up later.
-  for video in playlist_data['items']:
-    videos.append([
-      video['id'],
-      video['etag'],
-      video['contentDetails']['videoId']
-    ])
-
-
-  # Sub 1, since we already grabbed a page. This way, we don't grab more than
-  # we need.
-  for _ in range(0, total_pages - 1 ):
-    playlist_data = make_request(api.playlistItems().list(
-      playlistId=playlist_id,
-      part='contentDetails',
-      maxResults=max_results,
-      pageToken=next_page
-    ))
-
-    for video in playlist_data['items']:
-      videos.append([
-        video['id'],
-        video['etag'],
-        video['contentDetails']['videoId']
-      ])
-
-    # If the next pageToken is nil or blank, then we're done.
-    try:
-      next_page = playlist_data['nextPageToken']
-    except KeyError:
-      break
-    if next_page == "":
-      break
-
-  ids=''
+def get_video_data(api:gapi.Resource, id_list: list) -> list:
+  """Using a list of video ID's, return a list of titles and descriptions"""
+  ids = ''
+  out = []
   count = 0
-  for video_def in videos:
+  for vid in id_list:
     if count <= 49:
       if ids == "":
-        ids = video_def[2]
+        ids = vid
       else:
-        ids = "{0},{1}".format(ids, video_def[2])
+        ids = "%s,%s" % (ids, vid)
       count = count + 1 
     else:
       print("Getting %d videos: %s" % (count, ids), file=sys.stderr)
-
-      videos = make_request(api.videos().list(
+      response = make_request(api.videos().list(
         id=ids,
         part='snippet',
         maxResults=50
       ))
 
-      for data in videos['items']:
-        video_data.append([
+      for data in response['items']:
+        print("Added to list: %s" % (data['snippet']['title']), file=sys.stderr)
+        out.append([
           data['snippet']['title'],
           data['snippet']['description']
         ])
@@ -117,36 +65,133 @@ def main(api: googleapiclient.discovery.Resource, id: str):
 
   if count != 0:
     print("Getting %d videos: %s" % (count, ids), file=sys.stderr)
-    videos = make_request(api.videos().list(
+    response = make_request(api.videos().list(
       id=ids,
       part='snippet',
       maxResults=50
     ))
 
-    for data in videos['items']:
-      video_data.append([
+    for data in response['items']:
+      print("Added to list: %s" % (data['snippet']['title']), file=sys.stderr)
+      out.append([
         data['snippet']['title'],
         data['snippet']['description']
       ])
+  return out
 
-  drop_re = r'(https?://(?:www\.)?dropbox.com/s/[0-9a-zA-Z]+/[0-9a-z%A-Z ]+?.[mM][pP]3(:\?dl=[0-9]+)?)'
 
-  for data in video_data:
-    m = re.search(drop_re, data[1])
-    if m:
-      print(m.group(1))
+def output_video_data(m_re: re.Pattern, video_data: list) -> bool:
+  """Output all strings that match a given regex from a list of strings"""
+  print("Processing %d videos: %s" % (len(video_data), m_re), file=sys.stderr)
+  if len(video_data) > 0:
+    for data in video_data:
+      m = m_re.search(data[1])
+      if m is not None:
+        print(m[0])
+  else:
+    print("output_video_data: got empty data list", file=sys.stderr)
+    return False
+  return True
+
+
+def main(api: gapi.Resource, relist: list, id: str):
+  video_ids = []
+
+  # We only need the first item, since we're operating on one channel
+  print("Starting scrape", file=sys.stderr)
+  print("Getting channel: %s" % id, file=sys.stderr)
+  channel = make_request(api.channels().list(part="contentDetails", id=id))['items'][0]
+
+  # Get the uploaded videos playlist
+  playlist_id = channel['contentDetails']['relatedPlaylists']['uploads']
+  print("Getting playlist ID: %s" % playlist_id, file=sys.stderr)
+  playlist = make_request(api.playlistItems().list(
+    playlistId=playlist_id,
+    part='contentDetails',
+    maxResults=10
+  ))
+
+  next_page     = playlist['nextPageToken']
+  total_results = playlist['pageInfo']['totalResults']
+  results_per   = playlist['pageInfo']['resultsPerPage']
+  total_pages   = total_results // results_per
+
+  if total_results < 1:
+    print("The channel %s has no uploaded videos" % (id), file=sys.stderr)
+    exit(0)
+
+  # Just append to an array. We can clean this up later.
+  for video in playlist['items']:
+    video_ids.append(video['contentDetails']['videoId'])
+
+  # Sub 1, since we already grabbed a page. This way, we don't grab more than
+  # we need.
+  for _ in range(0, total_pages - 1 ):
+    playlist = make_request(api.playlistItems().list(
+      playlistId=playlist_id,
+      part='contentDetails',
+      maxResults=10,
+      pageToken=next_page
+    ))
+
+    for v in playlist['items']:
+      video_ids.append(v['contentDetails']['videoId'])
+
+    # If the next pageToken is nil or blank, then we're done.
+    try:
+      next_page = playlist['nextPageToken']
+    except KeyError:
+      break
+    if next_page == "":
+      break
+
+  [output_video_data(re, get_video_data(api, video_ids)) for re in relist]
 
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description=program_description)
+
+  parser.add_argument("--regex", "-r", 
+    metavar='<regex>', type=str, action='append')
+  parser.add_argument("--channel-id", "-c",
+    metavar='<channel>', type=str, action='store')
+  parser.add_argument("--api-key", "-k",
+    metavar='<key>', type=str, action='store')
+
+  args = parser.parse_args()
+
   try:
-    key = os.environ['API_KEY']
-    channel = os.environ['CHANNEL_ID']
-  except KeyError:
-    print("Please supply a valid API key (API_KEY), and channel (CHANNEL_ID)",
-      file=sys.stderr)
-    exit(1)
+    key = args.key
+  except AttributeError:
+    try:
+      key = os.environ['API_KEY']
+    except KeyError:
+      print("Please supply a valid API key, and channel.",
+        file=sys.stderr)
+      exit(1)
   
   try:
-    main(id=channel, api=build("youtube", "v3", developerKey=key))
-  except google.auth.exceptions.DefaultCredentialsError as error:
+    channel = args.channel
+  except AttributeError:
+    try:
+      channel = os.environ['CHANNEL_ID']
+    except KeyError:
+      print("Please supply a valid API key, and channel.",
+        file=sys.stderr)
+      exit(1)
+
+  try:
+    regex = args.regex
+  except AttributeError:
+    print("Please supply a valid regex capture.",
+      file=sys.stderr)
+    exit(1)
+
+  regex_list = [re.compile(capture) for capture in args.regex]
+
+  try:
+    main(id=channel, relist=regex_list,
+      api=gapi.build("youtube", "v3", developerKey=key))
+  except google.auth.exceptions.DefaultCredentialsError:
     print("Please supply a valid API key (API_KEY)", file=sys.stderr)
+    exit(1)
